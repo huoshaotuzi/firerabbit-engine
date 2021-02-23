9807da6f24203a57bc65c5ee85089ae9ed61339c
火烧兔子 <874811226@qq.com>
2021-02-23 12:20:01 +0800

    随机字符串

--- src/function.php
+++ src/function.php
@@ -1,5 +1,21 @@
 <?php
 /**
+ * 生成随机字符串
+ * @param $length
+ * @return false|string
+ */
+function str_random($length)
+{
+    $seeds = str_split('abcdefghijklmnopqrstuvwxyz1234567890');
+    shuffle($seeds);
+
+    $seeds = implode('', $seeds);
+    $str = substr($seeds, 0, $length);
+
+    return $str;
+}
+
+/**
  * 根据路由名称生成对应路由
  * @param $routeName
  * @param array $params
