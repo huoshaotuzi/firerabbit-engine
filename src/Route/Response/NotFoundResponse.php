831943beb4830b0d69376f0c4f94dd0faa9428ea
火烧兔子 <874811226@qq.com>
2021-04-15 10:14:21 +0800

    404页面

--- src/Route/Response/NotFoundResponse.php
+++ src/Route/Response/NotFoundResponse.php
@@ -6,15 +6,16 @@
  * Time：2:25 PM
  **/
 
-
 namespace FireRabbit\Engine\Route\Response;
 
+use FireRabbit\Engine\View\Blade;
 
 class NotFoundResponse extends RouteResponse
 {
     public function response($request, $response, $route)
     {
         $response->header("Content-Type", "text/html; charset=utf-8");
-        $response->end('不存在页面，404');
+        $content = Blade::view('error.404', []);
+        $response->end($content);
     }
 }
